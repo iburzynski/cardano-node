9e3440d42f4baf4951965875dd8555621b33f9be
John Ky <john.ky@iohk.io>
2023-04-07 13:30:39 +1000

    Modify genTx to have an era dependent type signature.  The function does not yet behave era dependently

--- cardano-api/test/Test/Cardano/Api/Typed/CBOR.hs
+++ cardano-api/test/Test/Cardano/Api/Typed/CBOR.hs
@@ -180,9 +180,9 @@ prop_roundtrip_ScriptData_CBOR = H.property $ do
 
 prop_roundtrip_UpdateProposal_CBOR :: Property
 prop_roundtrip_UpdateProposal_CBOR = H.property $ do
-  x <- H.forAll genUpdateProposal
-  H.trippingCbor AsUpdateProposal x
-
+  AnyCardanoEra era <- H.forAll $ Gen.element [minBound .. maxBound]
+  proposal <- H.forAll $ genUpdateProposal era
+  H.trippingCbor AsUpdateProposal proposal
 
 test_roundtrip_Tx_Cddl :: [TestTree]
 test_roundtrip_Tx_Cddl =
